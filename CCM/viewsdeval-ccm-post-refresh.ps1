90e4347b1fc495a4fbe80f72579f5b88eac6ece2
Dylan Egnoske <dylan.egnoske@va.gov>
2022-06-30 12:23:45 -0500

    updates

--- CCM/viewsdeval-ccm-post-refresh.ps1
+++ CCM/viewsdeval-ccm-post-refresh.ps1
@@ -1,14 +1,14 @@
-sfdx force:data:bulk:upsert -s Date__c -f "Dates_Data.csv" -i ID 
-sfdx force:data:bulk:upsert -s AD_Account__c -f "AD Stations Facility Upsert With Record.csv" -i ID
+sfdx force:data:bulk:upsert -s Date__c -f "data/Dates_Data.csv" -i ID 
+sfdx force:data:bulk:upsert -s AD_Account__c -f "data/AD Stations Facility Upsert With Record.csv" -i ID
 sfdx force:apex:execute -f schedule-batch-jobs.apex
 sfdx force:source:deploy -m Workflow:Case
-sfdx force:apex:execute -f create-contacts.apex   
-sfdx force:apex:execute -f create-users-viewsdeval.apex
-sfdx force:apex:execute -f create-accounts.apex
-sfdx force:apex:execute -f create-contacts-viewsdeval.apex
-sfdx force:apex:execute -f group-assign-viewsdeval.apex
-sfdx force:apex:execute -f primary-office-accounts.apex
-sfdx force:apex:execute -f primary-office-contacts-1.apex
-sfdx force:apex:execute -f primary-office-contacts-2.apex
-sfdx force:apex:execute -f primary-office-contacts-3.apex
-sfdx force:apex:execute -f setup-tiger-team-fq.apex      
+sfdx force:apex:execute -f apex/create-contacts.apex   
+sfdx force:apex:execute -f apex/create-users-viewsdeval.apex
+sfdx force:apex:execute -f apex/create-accounts.apex
+sfdx force:apex:execute -f apex/create-contacts-viewsdeval.apex
+sfdx force:apex:execute -f apex/group-assign-viewsdeval.apex
+sfdx force:apex:execute -f apex/primary-office-accounts.apex
+sfdx force:apex:execute -f apex/primary-office-contacts-1.apex
+sfdx force:apex:execute -f apex/primary-office-contacts-2.apex
+sfdx force:apex:execute -f apex/primary-office-contacts-3.apex
+sfdx force:apex:execute -f apex/setup-tiger-team-fq.apex      

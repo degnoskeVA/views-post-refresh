a8f4c4841a871c2f5efb3cde618f868f0deb9b62
degnoskeVA <103440839+degnoskeVA@users.noreply.github.com>
2022-06-30 10:53:11 -0500

    Update viewsdeval-authenticate-salesforce-authurl.ps1

--- .github/workflows/viewsdeval-authenticate-salesforce-authurl.ps1
+++ .github/workflows/viewsdeval-authenticate-salesforce-authurl.ps1
@@ -1,1 +1,13 @@
 
+Write-Host "AUTHENTICATING SALESFORCE ORG (VIA AUTH URL)" 
+$org_alias = "viewsdeval"
+$auth_url = "force://PlatformCLI::5Aep861fP5rIorteAvv7trvXXEGZdWIS_ruk46bTNN44ITgc9VwwKBN.Obgtdx9aWeE02F7DwF408hDg4UrfGLK@va--viewsdeval.my.salesforce.com"
+$key_file = "auth_url.key"
+
+New-Item -Type File $key_file | Out-Null
+$auth_url | Out-File $key_file
+
+$auth_result_json = $(sfdx auth:sfdxurl:store -f $key_file -a $org_alias --json)
+Write-Host $auth_result_json
+$auth_result = $auth_result_json| ConvertFrom-Json
+$auth_result.status
